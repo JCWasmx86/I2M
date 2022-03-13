b2387671e908aa55088a1f0c4475719d609c78e3
JCWasmx86 <JCWasmx86@t-online.de>
2022-03-13 20:21:29 +0100

    Formatting

--- src/main/java/io/jcwasmx86/i2m/App.java
+++ src/main/java/io/jcwasmx86/i2m/App.java
@@ -1,8 +1,12 @@
 package io.jcwasmx86.i2m;
 
-import io.github.ma1uta.matrix.client.StandaloneClient;
-import io.github.ma1uta.matrix.event.RoomEvent;
-import io.github.ma1uta.matrix.event.message.Text;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
+import com.google.gson.GsonBuilder;
+
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
@@ -11,14 +15,17 @@ import org.pircbotx.hooks.Listener;
 import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.QuitEvent;
-import com.google.gson.GsonBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.concurrent.Executors;
+import io.github.ma1uta.matrix.client.StandaloneClient;
+import io.github.ma1uta.matrix.event.RoomEvent;
+import io.github.ma1uta.matrix.event.message.Text;
 
 public class App {
+	@Override
+	public String toString() {
+		return "App []";
+	}
+
 	static class I2MConfiguration {
 		public String homeserverHostname;
 		public String botName;
@@ -29,37 +36,39 @@ public class App {
 		public String matrixChannelName;
 		public String matrixUserName;
 	}
-	static class AppListener implements Listener {
 
+	static class AppListener implements Listener {
 		private final StandaloneClient client;
 		private final long startTime;
-		private String id;
+		private final String id;
 		private PircBotX bot;
-		private I2MConfiguration config;
+		private final I2MConfiguration config;
 
-		public AppListener(StandaloneClient client, I2MConfiguration config) {
+		public AppListener(final StandaloneClient client, final I2MConfiguration config) {
 			this.client = client;
 			this.config = config;
 			this.id = this.client.room().joinByIdOrAlias(config.matrixChannelName, null, null).getRoomId();
 			this.startTime = System.currentTimeMillis();
-			var executor = Executors.newFixedThreadPool(1);
+			final var executor = Executors.newFixedThreadPool(1);
 			executor.submit(() -> {
 				String batch = null;
 				while (true) {
-					var response = this.client.sync().sync(null, batch, true, "online", 10 * 1000l);
+					final var response = this.client.sync().sync(null, batch, true, "online",
+							10 * 1000l);
 					batch = response.getNextBatch();
-					var rooms = response.getRooms();
+					final var rooms = response.getRooms();
 					if (rooms == null)
 						continue;
-					var join = rooms.getJoin();
+					final var join = rooms.getJoin();
 					if (join == null)
 						continue;
-					for (var room : join.entrySet()) {
-						var joined = room.getValue();
-						if (joined.getState() != null && joined.getState().getEvents() != null) {
+					for (final var room : join.entrySet()) {
+						final var joined = room.getValue();
+						if (joined.getState() != null
+								&& joined.getState().getEvents() != null) {
 							joined.getState().getEvents().forEach(this::evalEvent);
 						}
-						var timeline = joined.getTimeline();
+						final var timeline = joined.getTimeline();
 						if (timeline != null && timeline.getEvents() != null) {
 							timeline.getEvents().forEach(this::evalEvent);
 						}
@@ -69,13 +78,13 @@ public class App {
 			});
 		}
 
-		private void evalEvent(io.github.ma1uta.matrix.event.Event event) {
+		private void evalEvent(final io.github.ma1uta.matrix.event.Event event) {
 			if (event instanceof RoomEvent<?>) {
-				var re = (RoomEvent<?>)event;
-				var content = re.getContent();
+				final var re = (RoomEvent<?>) event;
+				final var content = re.getContent();
 				if (content instanceof Text) {
-					var rmc = (Text)content;
-					var body = rmc.getBody();
+					final var rmc = (Text) content;
+					final var body = rmc.getBody();
 
 					if (!re.getSender().equals(this.config.matrixUserName)) {
 						return;
@@ -89,39 +98,43 @@ public class App {
 		}
 
 		@Override
-		public void onEvent(Event event) throws Exception {
+		public void onEvent(final Event event) throws Exception {
 			if (event instanceof MessageEvent) {
-				var me = (MessageEvent)event;
-				var messageAsString = "[" + me.getUser().getNick() + "] " + me.getMessage();
+				final var me = (MessageEvent) event;
+				final var messageAsString = "[" + me.getUser().getNick() + "] " + me.getMessage();
 				this.client.event().sendMessage(this.id, messageAsString);
 			} else if (event instanceof JoinEvent) {
-				var je = (JoinEvent)event;
-				var notice = je.getUser().getNick() + " joined!";
+				final var je = (JoinEvent) event;
+				final var notice = je.getUser().getNick() + " joined!";
 				this.client.event().sendNotice(this.id, notice);
 			} else if (event instanceof QuitEvent) {
-				var qe = (QuitEvent)event;
-				var notice = qe.getUser().getNick() + " left: " + qe.getReason();
+				final var qe = (QuitEvent) event;
+				final var notice = qe.getUser().getNick() + " left: " + qe.getReason();
 				this.client.event().sendNotice(this.id, notice);
 			}
 		}
 
-		public void setBot(PircBotX bot) {
+		public void setBot(final PircBotX bot) {
 			this.bot = bot;
 		}
+
 	}
 
-	public static void main(String[] args) throws IOException, IrcException {
-		var gsonBuilder = new GsonBuilder ();
-		var gson = gsonBuilder.create ();
+	public static void main(final String[] args) throws IOException, IrcException {
+		final var gsonBuilder = new GsonBuilder();
+		final var gson = gsonBuilder.create();
 		I2MConfiguration config;
-		try(var bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.home") + "/.i2m.json"))) {
+		try (var bufferedReader = new BufferedReader(
+				new FileReader(System.getProperty("user.home") + "/.i2m.json"))) {
 			config = gson.fromJson(bufferedReader, I2MConfiguration.class);
 		}
-		var client = new StandaloneClient.Builder().domain(config.homeserverHostname).build();
-		var response = client.auth().login(config.botName, config.botPassword.toCharArray());
-		var listener = new AppListener(client, config);
-		var ircConfig = new Configuration.Builder().setName(config.ircUserName).addServer(config.ircHostName).addAutoJoinChannel(config.channelToJoin).addListener(listener).buildConfiguration();
-		var bot = new PircBotX(ircConfig);
+		final var client = new StandaloneClient.Builder().domain(config.homeserverHostname).build();
+		final var response = client.auth().login(config.botName, config.botPassword.toCharArray());
+		final var listener = new AppListener(client, config);
+		final var ircConfig = new Configuration.Builder().setName(config.ircUserName)
+				.addServer(config.ircHostName)
+				.addAutoJoinChannel(config.channelToJoin).addListener(listener).buildConfiguration();
+		final var bot = new PircBotX(ircConfig);
 		listener.setBot(bot);
 		bot.startBot();
 	}
