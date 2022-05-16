37f3205b834f8fa697332a065b82e9e4da94a615
JCWasmx86 <JCWasmx86@t-online.de>
2022-05-16 14:16:25 +0200

    Added more things

--- src/main/java/io/jcwasmx86/i2m/App.java
+++ src/main/java/io/jcwasmx86/i2m/App.java
@@ -3,16 +3,17 @@ package io.jcwasmx86.i2m;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
-import java.util.logging.Logger;
 
 import com.google.gson.GsonBuilder;
-
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
+import io.github.ma1uta.matrix.event.message.Emote;
+import io.github.ma1uta.matrix.event.message.Notice;
+import io.github.ma1uta.matrix.event.message.ServerNotice;
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
@@ -35,6 +36,71 @@ public class App {
 	static class I2MConfiguration {
 		public String homeserverHostname;
 		public String botName;
+
+		public String getHomeserverHostname() {
+			return homeserverHostname;
+		}
+
+		public void setHomeserverHostname(final String homeserverHostname) {
+			this.homeserverHostname = homeserverHostname;
+		}
+
+		public String getBotName() {
+			return botName;
+		}
+
+		public void setBotName(final String botName) {
+			this.botName = botName;
+		}
+
+		public String getBotPassword() {
+			return botPassword;
+		}
+
+		public void setBotPassword(final String botPassword) {
+			this.botPassword = botPassword;
+		}
+
+		public String getIrcUserName() {
+			return ircUserName;
+		}
+
+		public void setIrcUserName(final String ircUserName) {
+			this.ircUserName = ircUserName;
+		}
+
+		public String getIrcHostName() {
+			return ircHostName;
+		}
+
+		public void setIrcHostName(final String ircHostName) {
+			this.ircHostName = ircHostName;
+		}
+
+		public String getChannelToJoin() {
+			return channelToJoin;
+		}
+
+		public void setChannelToJoin(final String channelToJoin) {
+			this.channelToJoin = channelToJoin;
+		}
+
+		public String getMatrixChannelName() {
+			return matrixChannelName;
+		}
+
+		public void setMatrixChannelName(final String matrixChannelName) {
+			this.matrixChannelName = matrixChannelName;
+		}
+
+		public String getMatrixUserName() {
+			return matrixUserName;
+		}
+
+		public void setMatrixUserName(final String matrixUserName) {
+			this.matrixUserName = matrixUserName;
+		}
+
 		public String botPassword;
 		public String ircUserName;
 		public String ircHostName;
@@ -66,7 +132,7 @@ public class App {
 				String batch = null;
 				while (true) {
 					final var response = this.client.sync().sync(null, batch, true, "online",
-							10 * 1000l);
+						10 * 1000l);
 					batch = response.getNextBatch();
 					final var rooms = response.getRooms();
 					if (rooms == null)
@@ -77,7 +143,7 @@ public class App {
 					for (final var room : join.entrySet()) {
 						final var joined = room.getValue();
 						if (joined.getState() != null
-								&& joined.getState().getEvents() != null) {
+							&& joined.getState().getEvents() != null) {
 							joined.getState().getEvents().forEach(this::evalEvent);
 						}
 						final var timeline = joined.getTimeline();
@@ -91,23 +157,39 @@ public class App {
 		}
 
 		private void evalEvent(final io.github.ma1uta.matrix.event.Event event) {
-			if (event instanceof RoomEvent<?>) {
-				final var re = (RoomEvent<?>) event;
-				final var content = re.getContent();
-				if (content instanceof Text) {
-					final var rmc = (Text) content;
-					final var body = rmc.getBody();
+			var content = event.getContent();
+			if (content instanceof Emote) {
+				var re = (RoomEvent<?>) event;
+				final var emote = (Emote) event.getContent();
+				final var body = emote.getBody();
 
-					if (!re.getSender().equals(this.config.matrixUserName)) {
-						return;
-					}
-					if (re.getOriginServerTs() <= this.startTime) {
-						return;
-					}
-					bytesReceived += body.getBytes().length;
-					this.messagesSent++;
-					bot.send().message(this.config.channelToJoin, body);
+				if (!re.getSender().equals(this.config.matrixUserName)) {
+					return;
 				}
+				if (re.getOriginServerTs() <= this.startTime) {
+					return;
+				}
+				bot.send().action(this.config.channelToJoin, body);
+			}
+			if (content instanceof Text) {
+				var re = (RoomEvent<?>) event;
+				final var txt = (Text) content;
+				final var body = txt.getBody();
+
+				if (!re.getSender().equals(this.config.matrixUserName)) {
+					return;
+				}
+				if (re.getOriginServerTs() <= this.startTime) {
+					return;
+				}
+				if (body.equalsIgnoreCase("@members")) {
+					this.client.event().sendFormattedMessage(this.id, "Members",
+						"Members:\n" + bot.getUserChannelDao().getChannel(this.config.channelToJoin).getUsers().stream().map(a -> "- " + a.getNick() + "\n").reduce("", (a, b) -> a + b));
+					return;
+				}
+				bytesReceived += body.getBytes().length;
+				this.messagesSent++;
+				bot.send().message(this.config.channelToJoin, body);
 			}
 		}
 
@@ -134,45 +216,51 @@ public class App {
 			}
 		}
 
-		public void setBot(PircBotX bot) {
+		public void setBot(final PircBotX bot) {
 			this.bot = bot;
 		}
 
 		@Override
-		public void handle(HttpExchange exchange) throws IOException {
+		public void handle(final HttpExchange exchange) throws IOException {
 			try {
-				var now = System.currentTimeMillis();
-				var uptimeInSeconds = (now - this.startTime) / 1000;
-				var times = uptimeInSeconds % (24 * 60 * 60);
-				var days = (uptimeInSeconds - times) / (24 * 60 * 60);
-				var hours = (times - (times % 3600)) / 3600;
-				var secondsLeft = times % 3600;
-				var minutes = (secondsLeft - (secondsLeft % 60)) / 60;
-				var seconds = (int) (secondsLeft % 60);
-				var timestamp = "%d days, %d hours, %d minutes, %d seconds".formatted((int) days, (int) hours, (int) minutes, seconds);
-				var html = new StringBuilder();
-				html.append("<!DOCTYPE html><html><head><meta charset=\"utf-8\" name=\"viewport\" content= \"width=device-width, initial-scale=1.0\"></head>").append("<body>");
+				final var now = System.currentTimeMillis();
+				final var uptimeInSeconds = (now - this.startTime) / 1000;
+				final var times = uptimeInSeconds % (24 * 60 * 60);
+				final var days = (uptimeInSeconds - times) / (24 * 60 * 60);
+				final var hours = (times - (times % 3600)) / 3600;
+				final var secondsLeft = times % 3600;
+				final var minutes = (secondsLeft - (secondsLeft % 60)) / 60;
+				final var seconds = (int) (secondsLeft % 60);
+				final var timestamp = "%d days, %d hours, %d minutes, %d seconds".formatted((int) days,
+					(int) hours, (int) minutes, seconds);
+				final var html = new StringBuilder();
+				html.append("<!DOCTYPE html><html><head><meta charset=\"utf-8\" name=\"viewport\" content= \"width=device-width, initial-scale=1.0\"></head>")
+					.append("<body>");
+
 				html.append("<b>Uptime: </b>").append(timestamp).append("<br>");
-				html.append("<b>Sent data: </b>").append(this.formatData(this.bytesSent)).append("<br>");
-				html.append("<b>Received data: </b>").append(this.formatData(this.bytesReceived)).append("<br>");
+
+				html.append("<b>Sent data: </b>").append(this.formatData(this.bytesSent))
+					.append("<br>");
+				html.append("<b>Received data: </b>").append(this.formatData(this.bytesReceived))
+					.append("<br>");
 				html.append("<b>Messages sent: </b>").append(this.messagesSent).append("<br>");
 				html.append("<b>Messages received: </b>").append(this.messagesReceived).append("<br>");
 				html.append("<b>Notices sent: </b>").append(this.noticesSent).append("<br>");
 				html.append("</body></html>");
-				var htmlResponse = html.toString();
+				final var htmlResponse = html.toString();
 				exchange.sendResponseHeaders(200, htmlResponse.length());
-				var outputStream = exchange.getResponseBody();
+				final var outputStream = exchange.getResponseBody();
 				outputStream.write(htmlResponse.getBytes());
 				outputStream.flush();
 				outputStream.close();
 				exchange.close();
-			} catch (Exception e) {
+			} catch (final Exception e) {
 				e.printStackTrace();
 				exchange.close();
 			}
 		}
 
-		private String formatData(long bytes) {
+		private String formatData(final long bytes) {
 			if (bytes < 1024)
 				return "%dB".formatted(bytes);
 			else if (bytes < 1024 * 1024)
@@ -190,13 +278,13 @@ public class App {
 		final var gson = gsonBuilder.create();
 		I2MConfiguration config;
 		try (var bufferedReader = new BufferedReader(
-				new FileReader(System.getProperty("user.home") + "/.i2m.json"))) {
+			new FileReader(System.getProperty("user.home") + "/.i2m.json"))) {
 			config = gson.fromJson(bufferedReader, I2MConfiguration.class);
 		}
 		HttpServer server;
 		try {
 			server = HttpServer.create(new InetSocketAddress(8000), 50);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 			System.exit(-5);
 			return;
@@ -206,11 +294,11 @@ public class App {
 			server.start();
 		}).start();
 		final var client = new StandaloneClient.Builder().domain(config.homeserverHostname).build();
-		final var response = client.auth().login(config.botName, config.botPassword.toCharArray());
+		client.auth().login(config.botName, config.botPassword.toCharArray());
 		final var listener = new AppListener(client, config, server);
 		final var ircConfig = new Configuration.Builder().setName(config.ircUserName)
-				.addServer(config.ircHostName)
-				.addAutoJoinChannel(config.channelToJoin).addListener(listener).buildConfiguration();
+			.addServer(config.ircHostName)
+			.addAutoJoinChannel(config.channelToJoin).addListener(listener).buildConfiguration();
 		final var bot = new PircBotX(ircConfig);
 		listener.setBot(bot);
 		bot.startBot();
